ac91e3c1065ef24a1e63023e0e33f7f583ac1aa0
lylynguyen <lylynguyen812@gmail.com>
2016-01-04 22:43:25 -0800

    test

--- popup.js
+++ popup.js
@@ -1,11 +1,29 @@
-var wishListApp = angular.module('wishListApp', [])
-wishListApp.factory('wishListFactory', function(){
-  return {
-    url: window.location.href
-  }
-});
+var app = angular.module('wishListApp', [])
+// var angular = require("angular")
+// var wishListApp = angular.module('wishListApp', [])
 
-wishListApp.controller('wishListCtrl', function($scope, wishListFactory){
-  $scope.url = wishListFactory.url;
 
-});
+
+// wishListApp.factory('wishListFactory', function(){
+//   return {
+//     url: window.location.href
+//   }
+// });
+
+// wishListApp.controller('wishListCtrl', function($scope, wishListFactory){
+//   $scope.url = wishListFactory.url;
+
+// });
+
+// var wishListApp = angular.module('wishListApp', [])
+//  wishListApp.factory('wishListFactory', function(){
+//    return {
+//      url: window.location.href
+//    }
+//  });
+ 
+//  wishListApp.controller('wishListCtrl', function($scope, wishListFactory){
+//    // $scope.url = wishListFactory.url;
+//    $scope.url = "hey"
+
+//  });
